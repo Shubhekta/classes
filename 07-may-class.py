da2f1107313ae737874014aa83ad8b54ce5adcb2
classes <shubham.gupta.devops.er@gmail.com>
2026-05-09 00:31:07 +0530

    .\07-may-class.py

--- 07-may-class.py
+++ 07-may-class.py
@@ -22,22 +22,56 @@
 # else:
 #     print("name is not provided")
 
-name="shubham" 
-if name:
-    print(f"yes name {name} is provided")
-    address="noida"
-    if address:
-        print(f"yes address is : {address} provided" )
+# name="shubham" 
+# if name:
+#     print(f"yes name {name} is provided")
+#     address="noida"
+#     if address:
+#         print(f"yes address is : {address} provided" )
 
-    else:
-        print("name is not provided")
+#     else:
+#         print("name is not provided")
 
-        num1=20
-        if num1%2==0:
-            print("EVEN")
-            mob=input("enter your number :") 
-if len(mob) ==10:
-    print("valid number") 
-else:
-    print("invalid number")  
+# num1=20
+# if num1%2==0:
+#     print("EVEN")
+#     mob=input("Enter your number :") 
+#     if len(mob) ==10:
+#         print("valid number") 
+#     else:
+#         print("invalid number")  
+# else:
+#     print("ODD")
 
+
+
+
+# name = "shubham"
+
+# if name:
+#     print(f"yes name {name} is provided")
+
+#     address = "noida"
+
+#     if address:
+#         print(f"yes address is : {address} provided")
+#     else:
+#         print("address is not provided")
+
+# else:
+#     print("name is not provided")
+
+
+# num1 = 20
+
+# if num1 % 2 == 0:
+#     print("EVEN")
+
+#     mob = input("enter your number : ")
+
+#     if len(mob) == 10:
+#         print("valid number")
+#     else:
+#         print("invalid number")
+# else:
+#     print("ODD")
--- 08-may.py
+++ 08-may.py
@@ -11,23 +11,139 @@
 #range (start:0,stop-1,step:1)
 
 #for i in range(5,10,1): 
-    #print(i,end="")
+    
 
+#1.for loop
+# range(start, stop, step) (0, 10, 1)
+# range always works with integer
+#for i in range(10):#underscore use for run the loop
+    #if i%2 !=0 #it print opposite of
+   # print(i, end = " ") #end will give you horizontally output
+    # print(i, end = " ")
+    # print("python", end= " " ) #end use for horizontally 
+     #if i == 10:
+       # continue # use for skip 10
+    #break # for terminate loop
+ #print(i)
 
+#s=0
+#for i in range (1, 5): 
+     # s = s+1
+      #print(s) #print continuosly 
+  #print(s) #total answer will print 
 
-# 1.Wap to takes start_point and end_point from user input 
-# and print all number divisble by 2 and 3.
+# for i in range(5):
+    # print(i)
 
-# 2.Wap to take a number from user input and print formated table.
-# format : 
-# 3x1=3
-# 3x2=6
-...
-# 3x10=30
+#for i in range(0,5,1):
+#    print(i) 
 
-# 3.Wap to take a number from user input and print reversed formated table.
-# format : 
-# 3x10=30
-# 3x2=6
-# 3x1=3
+#for i in range(3,11,1):
+#    print(i)
 
+#for i in range(3,11,2):
+#    print(i)
+
+#for i in range(3,11):
+#    print(i)
+
+#for i in range(11):
+#    print(i)
+
+#for i in range(1, 10, 1):
+   # print(i)
+
+#for i in range(5,10,1):
+   # print(i,end=" ")
+
+# for i in range(2,10,2):
+#     print(i,end=" ")
+
+# for i in range(5,10,1):
+#     print(i,end=" ")
+#     print("python") #Loop ke andar hai to horizontal ayega,
+
+# for i in range(5,10,1):
+#     print(i,end=" ")
+# print("python") #Loop ke bahar hai to vertical ayega,
+
+# for i in range(1,15):
+#     if i==6:
+#         continue
+#     print(i) 
+
+# for i in range(1,15):
+#     if i==6:
+#         break
+#     print(i) 
+
+#Doubt
+# for i in range(1,20):
+#     if i==22:
+#         break #break and continew loop ke andar use hota hai ,
+#     print(i)
+
+# for i in range(1,20):
+#     if i%2==0:
+#         print(i) #vertical ayega 
+
+# for i in range(1,20):
+#     if i%2==0:
+#         print(i,end=" , ") #end ka use karenge to Horizontal line ayega ,
+
+# for i in range(1,21):
+#     if i%2 != 0:
+#         print(i,end=" ")
+    
+# for i in range(1,21):
+#     if i%2 != 0:
+#         print(f"ODD : {i}")   
+#     else:
+#         print(f"EVEN : {i}")
+
+# s=0
+# for i in range(1,5):
+#     s=s+i
+#     print(s)
+
+# s=1
+# for i in range(1,5):
+#     s=s*i
+#     print(s)
+
+# for r in range(10,-1,-1):
+#    print(r)
+
+# for r in range(10,0,-1):
+#    print(r)
+
+# for r in range(10,-3,-1):
+#    print(r)
+
+# for i in range(1,15):
+#    if i>=6:
+#       print(i)
+
+# for i in range(1,15):
+#    if i<=6:
+#       print(i)
+
+# for i in range(1,15):
+#    if i>=6:
+#       continue
+#    print(i)
+
+# for i in range(1,15):
+#    if i==6:
+#       continue
+#    print(i,end=" ")
+
+# for i in range(1,15):
+#    if i==6:
+#       continue
+#    print(i)
+
+# for i in range(1,15):
+#    if i==6:
+#       continue
+#    print(i, "  💖")
